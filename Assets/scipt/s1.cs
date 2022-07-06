7c27b9f032f9059f556608ecdab4fc643c4f30f0
Kino <jaikino9527@gmail.com>
2022-07-06 11:37:26 +0800

    嘿嘿Debug

--- Assets/scipt/s1.cs
+++ Assets/scipt/s1.cs
@@ -7,7 +7,7 @@ public class s1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log();
     }
 
     // Update is called once per frame
